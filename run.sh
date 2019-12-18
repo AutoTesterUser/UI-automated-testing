d37a0b71b2118f46f0cafbf6c3493c80627ae824
wanghaitao <17634771792@163.com>
2019-12-18 12:48:12 +0800

    venv

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
-. /home/shdy/projects/UI-automated-testing/venv/bin/activate
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simpl --default-timeout=1000 --no-cache-dir -r requirements.txt
 python3 main.py
 #allure generate test-output - o allure-report --clean
