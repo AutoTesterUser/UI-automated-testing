a543344d7ea49748f7322a50280f036b1fc48686
wanghaitao <17634771792@163.com>
2019-12-18 12:50:41 +0800

    venv

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 python3 -m venv venv
+pip install --upgrade pip
 source venv/bin/activate
-pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simpl --default-timeout=1000 --no-cache-dir -r requirements.txt
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simpl --default-timeout=1000 --no-cache-dir -r requirements.txt
 python3 main.py
 #allure generate test-output - o allure-report --clean
